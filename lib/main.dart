65b9d01a32cb1db48f838f5418c0e91557b92b77
Annisa Eva <annisa.eat@gmail.com>
2021-07-23 03:28:01 +0700

    widget untuk membuat Heading (sebuah text yang digunakan untuk judul)

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,18 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading({this.text});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello, world!'),
-        ),
-        body: Center(
-          child: Text('Hello, world!'),
-        ),
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
